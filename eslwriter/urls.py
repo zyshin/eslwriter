3196976ed686ce0e60d881bc934312b5358811c6
ZYShin <yeshuangzhu@gmail.com>
2016-06-21 20:40:35 +0800

    Improve UI

--- eslwriter/urls.py
+++ eslwriter/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = [
-	url(r'^$', views.home_view, name='eslwriter'),
+    url(r'^$', views.home_view, name='eslwriter'),
     url(r'^dep/$', views.dep_query_view, name='dep'),
     url(r'^sentence/$', views.sentence_query_view, name='sentence'),
 ]
