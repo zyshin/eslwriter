8f3f6eab8a30ca47a8a5b6c30bbeba3d21654917
ZYShin <yeshuangzhu@gmail.com>
2017-02-20 12:31:47 +0800

    Fix send mail interval

--- scripts/send_mails.py
+++ scripts/send_mails.py
@@ -11,11 +11,11 @@ def send_activation_prompt_emails(receivers):
     for receiver in receivers:
         profile = RegistrationProfile.objects.get(user__email=receiver)
         RegistrationProfile.objects.resend_activation_mail(profile.user.email, site)
-        time.sleep(500)
+        time.sleep(120)
     del settings.ACTIVATION_EMAIL_HTML
 
 def send_emails(subject, message_txt, message_html, receivers):
     from_email = getattr(settings, 'REGISTRATION_DEFAULT_FROM_EMAIL', settings.DEFAULT_FROM_EMAIL)
     for receiver in receivers:
         send_mail(subject, message_txt, from_email, [receiver], html_message=message_html)
-        time.sleep(500)
+        time.sleep(120)
