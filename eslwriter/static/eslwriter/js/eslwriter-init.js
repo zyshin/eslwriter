2c4772841c69e3fb9dba4f9c10fb93c54655bad1
ZYShin <yeshuangzhu@gmail.com>
2017-03-09 21:22:14 +0800

    Add tutorial animation & remove colorbox

--- eslwriter/static/eslwriter/js/eslwriter-init.js
+++ eslwriter/static/eslwriter/js/eslwriter-init.js
@@ -1,57 +1,109 @@
-function delayedAction(sender, action, interval) {
-    if(!interval) {
-        interval = 500;
+$(document).ready(function() {
+    function delayedAction(sender, action, interval) {
+        if(!interval) {
+            interval = 500;
+        }
+        if(sender.promise_) {
+            clearTimeout(sender.promise_);
+        }
+        sender.promise_ = setTimeout(action, interval);
     }
-    if(sender.promise_) {
-        clearTimeout(sender.promise_);
-    }
-    sender.promise_ = setTimeout(action, interval);
-}
 
-var CACHE = {};
-function setupAutoComplete(url, inputId, resultId) {
-    $(inputId).autocomplete({
-        appendTo: resultId,
-        delay: 500,
-        minLength: 1,
-        source: function(request, response) {
-            var q = request.term;
-            // TODO
-            var qkey = String($('#CorpusSelector').val()) + ': ' + q;
-            if (qkey in CACHE) {
-                response(CACHE[qkey]);
-                return;
-            }
-            $.ajax({
-                url: url,
-                dataType: 'json',
-                data:{q: q, c: $('#CorpusSelector').val()},
-                success: function(r) {
-                    var data = $.map(r.gr, function(item){
-                        // TODO using template to render
-                        var s = item[1];
-                        var p1 = s.indexOf('<strong>'), p2 = s.lastIndexOf('</strong>');
-                        var begin = Math.max(p1 - Math.floor((74-(p2-p1-25))/2), 0);
-                        if(begin > 0 && s[begin] != ' ' && s[begin-1] != ' '){
-                            begin += s.slice(begin).indexOf(' ') + 1;
-                        }
-                        s = s.slice(begin);
-                        if(begin > 0)
-                            s = '...' + s;
-                        var label = '<p class="suggestWord textOverflow">'+ item[0] + '</p>' + '<p class="exampleSentence textOverflow">' + s + '</p>';
-                        return {value: item[0], label: label};
-                    });
-                    CACHE[qkey] = data;
-                    response(data);
-                },
-                fail: function() {
-                    response([]);
+    var CACHE = {};
+    function setupAutoComplete(url, inputId, resultId) {
+        $(inputId).autocomplete({
+            appendTo: resultId,
+            delay: 500,
+            minLength: 1,
+            source: function(request, response) {
+                var q = request.term;
+                // TODO
+                var qkey = String($('#CorpusSelector').val()) + ': ' + q;
+                if (qkey in CACHE) {
+                    response(CACHE[qkey]);
+                    return;
                 }
-            });
-        },
-        html: true
+                $.ajax({
+                    url: url,
+                    dataType: 'json',
+                    data:{q: q, c: $('#CorpusSelector').val()},
+                    success: function(r) {
+                        var data = $.map(r.gr, function(item){
+                            // TODO using template to render
+                            var s = item[1];
+                            var p1 = s.indexOf('<strong>'), p2 = s.lastIndexOf('</strong>');
+                            var begin = Math.max(p1 - Math.floor((74-(p2-p1-25))/2), 0);
+                            if(begin > 0 && s[begin] != ' ' && s[begin-1] != ' '){
+                                begin += s.slice(begin).indexOf(' ') + 1;
+                            }
+                            s = s.slice(begin);
+                            if(begin > 0)
+                                s = '...' + s;
+                            var label = '<p class="suggestWord textOverflow">'+ item[0] + '</p>' + '<p class="exampleSentence textOverflow">' + s + '</p>';
+                            return {value: item[0], label: label};
+                        });
+                        CACHE[qkey] = data;
+                        response(data);
+                    },
+                    fail: function() {
+                        response([]);
+                    }
+                });
+            },
+            html: true
+        });
+    }
+    if($('#SearchInput').length>0){
+        setupAutoComplete(DEP_URL, '#SearchInput', '#SuggestionContainer');
+        $('#SearchInput').focus();
+    }
+    else if($('#NavSearchInput').length>0){
+        setupAutoComplete(DEP_URL, '#NavSearchInput', '#SuggestionContainer');
+        $('#NavSearchInput').focus();
+    }
+
+    var ANIMATION_ON = false;
+    function fillWithExample(text) {
+        // scroll to top
+        // clear and fill in text
+        // (wait drop down and select suggestion)
+        // click search button
+        if (ANIMATION_ON || !text) {
+            return;
+        }
+        ANIMATION_ON = true;
+        var showAutocomplete = (text.indexOf('*') >= 0 || text.indexOf('(') >= 0);
+        var q = text.replace(/\*/g, '').replace(/\(.*?\)/g, '').trim();
+        $('body').animate({ scrollTop: 0 }, 'fast', 'swing', function() {
+            $('#SearchInput').val('');
+            (function addChar() {
+                if (q) {
+                    $('#SearchInput').val($('#SearchInput').val() + q[0]);
+                    q = q.slice(1);
+                    setTimeout(addChar, 80);
+                } else {
+                    ANIMATION_ON = false;
+                    $('#SearchInput').focus();
+                    if (showAutocomplete) {
+                        $('#SearchInput').autocomplete('search');
+                        setTimeout(function() {
+                            $('#SearchInput').val(text);
+                        }, 1000);
+                    }
+                    setTimeout(function() {
+                        $('#SearchBtn').toggleClass('active');
+                        $('#SearchBtn').click();
+                    }, 1500);
+                }
+            })();
+        });
+    }
+
+    $('.example-btn-link').click(function() {
+        fillWithExample($(this).text());
     });
-}
+});
+
 
 /*
  * jQuery UI Autocomplete HTML Extension
