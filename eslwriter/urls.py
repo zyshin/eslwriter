1ba47d66d22e81c4388a2973619d87d763d435f1
ZYShin <yeshuangzhu@gmail.com>
2017-03-09 22:02:01 +0800

    Add template for guide page

--- eslwriter/urls.py
+++ eslwriter/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 import views
 
 urlpatterns = [
     url(r'^$', views.home_view, name='eslwriter'),
     url(r'^dep/$', views.dep_query_view, name='dep'),
     url(r'^sentence/$', views.sentence_query_view, name='sentence'),
+    url(r'^guide/$', TemplateView.as_view(template_name='eslwriter/guide.html'), name='eslwriter_guide'),
 ]
